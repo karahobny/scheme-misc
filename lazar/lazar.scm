6a351225f5f0c5a3bf9f4b818aafdb3a050e20c5
karahobny <karahobny@gmail.com>
2018-05-13 00:06:21 +0300

    just cleaning it up a bit

--- lazar/lazar.scm
+++ lazar/lazar.scm
@@ -1,12 +1,15 @@
 (library (lazar)
   (export
    ;; aux keywords λ.=> est ε case-with)
-   => .. || else rec in
+   => .. || else rec in : ->
    ;; syntax
-   λ λ.=> ε case-with Pair ListOf Vec VecOf
-   Fn Bool Symbol Char Str Num Natural
-   natural? Int Real Rational Complex Any
-   a' α ℕ ℤ ℝ ℚ ℂ : -> define/c)
+   λ λ.=> ε case-with
+   ;; types / contracts
+   Pair ListOf Vec VecOf
+   Fn Bool Symbol Char Str Num
+   Natural natural? Int Real
+   Rational Complex Any a'
+   α ℕ ℤ ℝ ℚ ℂ  define/c)
   (import (rnrs (6))
           (lazar fn-lambda)
           (lazar est)
